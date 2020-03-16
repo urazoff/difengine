f1701879da656a0a9211b7f429c1f51a46f078be
Dilshod Urazov <dilshod@difengine.xyz>
2020-03-16 23:45:34 +0300

    objects: add general object

---
+++ include/object.h
@@ -0,0 +1,30 @@
+#ifndef _DF_OBJECT_H_
+#define _DF_OBJECT_H_
+
+/**
+ * @file
+ * General object interface
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define Df_size_t size_t
+#define DfType void*
+
+#define DfDLList \
+    struct df_object *next; \
+    struct df_object *prev
+
+typedef struct df_object
+{   DfDLList;
+    Df_size_t rfcnt;
+    DfType    type;
+} DfObject;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DF_OBJECT_H_ */
