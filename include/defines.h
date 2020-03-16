237ddab6e92b65f192fc3ea88de4d474bd9d5053
Dilshod Urazov <dilshod@difengine.xyz>
2020-03-16 23:44:28 +0300

    common: add common UNUSED macro

---
+++ include/defines.h
@@ -0,0 +1,22 @@
+#ifndef _DF_DEFINES_H_
+#define _DF_DEFINES_H_
+
+/**
+ * @file
+ * Common macro defines
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Avoid compiler warning on unused variables.
+ */
+#define UNUSED(_x) (void)(_x)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DF_DEFINES_H_ */
