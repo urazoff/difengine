79c6b223065f7f68190dbd3c9b318af8728158d2
Dilshod Urazov <dilshod@difengine.xyz>
2020-03-26 01:07:23 +0300

    common: add meta-include header

---
+++ include/difengine.h
@@ -0,0 +1,13 @@
+#ifndef _DF_DEFINES_H_
+#define _DF_DEFINES_H_
+
+/**
+ * @file
+ * Meta-include header file
+ */
+
+#include <stdbool.h> 
+#include <stddef.h>  
+#include <stdint.h>  
+
+#endif /* DF_DIFENGINE_H_ */
