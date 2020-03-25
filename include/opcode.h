30446d2d7e5892004bccd9f336071c580371297e
Dilshod Urazov <dilshod@difengine.xyz>
2020-03-26 01:08:07 +0300

    opcodes: add initial opcodes enumerator

---
+++ include/opcode.h
@@ -0,0 +1,21 @@
+#ifndef _DF_OPCODE_H_
+#define _DF_OPCODE_H_
+
+/**
+ * @file
+ * Operation's codes
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    RETURN_VALUE
+} Op_Codes;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DF_OPCODE_H_ */
