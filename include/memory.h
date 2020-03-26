41878a95b01536b974727caa3212f711e9fbdf2d
Dilshod Urazov <dilshod@difengine.xyz>
2020-03-26 23:39:58 +0300

    memory: define simple free macros

--- include/memory.h
+++ include/memory.h
@@ -10,9 +10,18 @@
 extern "C" {
 #endif
 
-#define DF_MEM_GROW_ARRAY(_type, _prev, _old_num, _num) \
-        (_type*) df_realloc(_prev, sizeof(_type) * _old_num, \
-                            sizeof(_type) * _num)
+#define DF_MEM_ALLOC(_size) \
+    df_realloc(NULL, 0, _size);
+
+#define DF_MEM_FREE(_ptr) \
+    df_realloc(_ptr, 0, 0);
+
+#define DF_MEM_FREE_ARRAY(_type, _ptr, _num) \
+    df_realloc(_ptr, sizeof(_type) * _num, 0);
+
+#define DF_MEM_GROW_ARRAY(_type, _prev, _old_num, _num)  \
+    (_type*) df_realloc(_prev, sizeof(_type) * _old_num, \
+                        sizeof(_type) * _num)
 
 void *df_realloc(void *prev, size_t old_size, size_t new_size);
 
