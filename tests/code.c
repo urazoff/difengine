8c844265e4991c0262219d4d751540a8c8304561
Dilshod Urazov <dilshod@difengine.xyz>
2020-03-29 16:19:41 +0300

    tests: apply code object changes
    Code object opcodes is list now.

--- tests/code.c
+++ tests/code.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 #include "difengine.h"
+#include "intobject.h"
 #include "opcode.h"
 #include "code.h"
 #include "dis.h"
+#include "memory.h"
 
 int main(int argc, char *argv[])
 {
     DfCodeObj *code;
+    DfIntObj *opcode;
+    DfIntObj *const_obj = DF_MEM_ALLOC(sizeof(DfIntObj));
+    int i;
 
     code = df_code_obj_init();
 
-    df_code_obj_upd(code, RETURN_VALUE);
+    df_code_obj_add_op(code, RETURN_VALUE);
+    opcode = (DfIntObj *)code->opcodes->items[code->opcodes->count - 1]; 
 
     printf("Code:\n---- count: %u \n"
            "---- capacity %u \n"
-           "---- opcode: %u \n",
-           code->count,
-           code->capacity,
-           code->opcodes[code->count - 1]);
+           "---- opcode: %d \n",
+           code->opcodes->count,
+           code->opcodes->capacity,
+           opcode->val);
+
+    i =  df_code_obj_add_const(code, (DfObject *)const_obj);
+    printf("Consts index: ---- i = %d\n", i);
 
     df_dis_code_obj(code, "test_code");
 
-    df_code_obj_free(code);
+    df_code_obj_clear(code);
 
     printf("Success");
 }
