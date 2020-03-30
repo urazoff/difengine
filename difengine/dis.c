d0c93f4cc03a8220f1a6305485f7b6fdf17c24fc
Dilshod Urazov <dilshod@difengine.xyz>
2020-03-30 16:50:55 +0300

    dis: disassemble LOAD_CONST

--- difengine/dis.c
+++ difengine/dis.c
@@ -12,6 +12,23 @@ df_dis_op(const char *op_name, int offset)
     return ++offset;
 }
 
+static int
+df_dis_load_const(const char *name, DfCodeObj *code, int offset)
+{
+    DfIntObj *operand;
+    DfObject *const_obj;
+    uint8_t const_index;
+
+    operand = (DfIntObj *)code->opcodes->items[offset + 1];
+    const_index = operand->val;
+    const_obj = code->consts->items[const_index];
+
+    /* There are no types yet. So assume that const_obj is of int type. */
+    printf("%s %4d %d\n", name, const_index, ((DfIntObj *)const_obj)->val);
+
+    return offset + 2;
+}
+
 int
 df_dis_code_obj_op(DfCodeObj *code, int offset)
 {
@@ -24,6 +41,8 @@ df_dis_code_obj_op(DfCodeObj *code, int offset)
     {
         case RETURN_VALUE:
             return df_dis_op("RETURN_VALUE", offset);
+        case LOAD_CONST:
+            return df_dis_load_const("LOAD_CONST", code, offset);
         default:
             printf("Unknown opcode: %d", op->val);
             return ++offset;
