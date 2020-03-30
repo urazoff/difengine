69d0f4fd469bdb2c0b4a5b3f30e00837f9c235f7
Dilshod Urazov <dilshod@difengine.xyz>
2020-03-30 16:51:37 +0300

    tests: code: add LOAD_CONST

--- tests/code.c
+++ tests/code.c
@@ -9,24 +9,17 @@
 int main(int argc, char *argv[])
 {
     DfCodeObj *code;
-    DfIntObj *opcode;
     DfIntObj *const_obj = DF_MEM_ALLOC(sizeof(DfIntObj));
     int i;
 
     code = df_code_obj_init();
-
     df_code_obj_add_op(code, RETURN_VALUE);
-    opcode = (DfIntObj *)code->opcodes->items[code->opcodes->count - 1]; 
 
-    printf("Code:\n---- count: %u \n"
-           "---- capacity %u \n"
-           "---- opcode: %d \n",
-           code->opcodes->count,
-           code->opcodes->capacity,
-           opcode->val);
-
+    const_obj->val = 17;
     i =  df_code_obj_add_const(code, (DfObject *)const_obj);
-    printf("Consts index: ---- i = %d\n", i);
+
+    df_code_obj_add_op(code, LOAD_CONST);
+    df_code_obj_add_op(code, i);
 
     df_dis_code_obj(code, "test_code");
 
