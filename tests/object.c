2d97b7ac6e3264706a296147dbded16860184826
Dilshod Urazov <dilshod@difengine.xyz>
2020-04-01 17:07:41 +0300

    tests: object: make uniform with others /tests

--- tests/object.c
+++ tests/object.c
@@ -6,10 +6,14 @@ int main(int argc, char *argv[])
 {
     DfObject object;
 
+    printf("Object test starts...");
+
     object.rfcnt = 4;
 
     printf("Object layout:\n ---- rfcnt:  %zu \n",
            object.rfcnt);
 
     printf("Success");
+
+    return 0;
 }
