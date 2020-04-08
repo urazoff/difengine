174971b3e61ffef3ce0020fe1c0e5529de3257cd
Dilshod Urazov <dilshod@difengine.xyz>
2020-04-08 15:05:55 +0300

    tests: add stack frame object test

---
+++ tests/api/frame.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "difengine.h"
+#include "intobject.h"
+#include "opcode.h"
+#include "code.h"
+#include "frame.h"
+#include "dis.h"
+#include "memory.h"
+
+int main(int argc, char *argv[])
+{
+    DfCodeObj *code;
+    DfFrameObj *frame;
+    DfIntObj *const_obj = DF_MEM_ALLOC(sizeof(DfIntObj));
+    int i;
+
+    code = df_code_obj_init();
+    df_code_obj_add_op(code, RETURN_VALUE, 1);
+
+    const_obj->val = 17;
+    i =  df_code_obj_add_const(code, (DfObject *)const_obj);
+
+    df_code_obj_add_op(code, LOAD_CONST, 2);
+    df_code_obj_add_op(code, i, 2);
+
+    frame = df_frame_obj_init(code);
+
+    df_dis_code_obj(frame->code, "test_frame->code");
+
+    df_code_obj_clear(frame->code);
+
+    printf("RES: Success\n");
+
+    return 0;
+}
