urazoff/difengine

commit a4f2750855f1ce07b27fc2b517bbc80d37b463c6

memory: implement basic memory routine

include/memory.h
@@ -1,0 +1,23 @@
+#ifndef _DF_MEMORY_H_
+#define _DF_MEMORY_H_
+
+/**
+ * @file
+ * Memory allocation routine
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DF_MEM_GROW_ARRAY(_type, _prev, _old_num, _num) \
+        (_type*) df_realloc(_prev, sizeof(_type) * _old_num, \
+                            sizeof(_type) * _num)
+
+void *df_realloc(void *prev, size_t old_size, size_t new_size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DF_MEMORY_H_ */

commit d8f3b6ca1d11b4a607aa30e5c528f7c7f1e5330b

memory: fix headers inconsistency
Fixes: a4f2750855f1 ("memory: implement basic memory routine")
Cc: dilshod@difengine.xyz

difengine/memory.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include "difengine.h"
 #include "memory.h"
 
 void *

include/memory.h
@@ -1,6 +1,8 @@
 #ifndef _DF_MEMORY_H_
 #define _DF_MEMORY_H_
 
+#include "difengine.h"
+
 /**
  * @file
  * Memory allocation routine