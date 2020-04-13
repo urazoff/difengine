3641611da62d93a1f857d7c3b8e3081f2e09c0a1
Dilshod Urazov <dilshod@difengine.xyz>
2020-04-13 22:43:53 +0300

    object: frame: add value stack declaration

--- difengine/frame.c
+++ difengine/frame.c
@@ -7,6 +7,9 @@ df_frame_obj_init(DfCodeObj *code)
     DfFrameObj *frame = DF_MEM_ALLOC(sizeof(DfFrameObj));
 
     frame->code = code;
+    frame->stack = NULL;
+    frame->stack_top = NULL;
+    frame->stack_size = 0;
     frame->ip = 0;
 
     return frame;
--- include/frame.h
+++ include/frame.h
@@ -16,8 +16,11 @@ extern "C" {
 
 typedef struct {
     DfObject obj;
-    DfCodeObj *code; /* code object */
-    int ip;          /* instruction index about to be evaluated */
+    DfCodeObj *code;     /* code object */
+    DfObject **stack;    /* value stack */
+    DfObject *stack_top; /* points to location of an element to be pushed */
+    int stack_size;      /* value stack size */
+    int ip;              /* instruction index about to be evaluated */
 } DfFrameObj;
 
 /**
