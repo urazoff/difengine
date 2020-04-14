ce7249e4dd27cb77126d862d652c11319adc5ef1
Dilshod Urazov <dilshod@difengine.xyz>
2020-04-14 18:14:49 +0300

    object: frame: add stack allocation

--- difengine/frame.c
+++ difengine/frame.c
@@ -6,10 +6,12 @@ df_frame_obj_init(DfCodeObj *code)
 {
     DfFrameObj *frame = DF_MEM_ALLOC(sizeof(DfFrameObj));
 
+    frame->stack = DF_MEM_ALLOC(code->stack_size * sizeof(DfObject *));
+    frame->stack_size = code->stack_size;
+    /* top points to the bottom of the stack which is thus empty */
+    frame->stack_top = frame->stack;
+
     frame->code = code;
-    frame->stack = NULL;
-    frame->stack_top = NULL;
-    frame->stack_size = 0;
     frame->ip = 0;
 
     return frame;
