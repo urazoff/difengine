urazoff/difengine

commit 2653aae518678d596fb3252f56a20418d54e0312

object: add abstract object operations interface
Also simple binary add operation is implemented.

difengine/objectops.c
@@ -1,0 +1,12 @@
+#include "objectops.h"
+#include "intobject.h"
+#include "memory.h"
+
+DfObject*
+df_num_add(DfObject *a, DfObject *b)
+{
+    DfIntObj *sum = df_int_obj_new(((DfIntObj *)a)->val +
+                                   ((DfIntObj *)b)->val);
+
+    return sum;
+}

difengine/meson.build
@@ -12,9 +12,20 @@ frame_lib = static_library('frame',
                             include_directories: global_inc,
                             link_with: mem_lib)
 
+int_lib = static_library('int',
+                         'intobject.c',
+                          include_directories: global_inc,
+                          link_with: mem_lib)
+
+ops_lib = static_library('ops',
+                         'objectops.c',
+                          include_directories: global_inc,
+                          link_with: int_lib)
+
 eval_lib = static_library('evaluate',
                           'evaluate.c',
-                           include_directories: global_inc)
+                           include_directories: global_inc,
+                           link_with: ops_lib)
 
 list_lib = static_library('list',
                           'listobject.c',

include/objectops.h
@@ -1,0 +1,28 @@
+#ifndef _DF_OBJECTOPS_H_
+#define _DF_OBJECTOPS_H_
+
+#include "object.h"
+#include "difengine.h"
+
+/**
+ * @file
+ * Abstract object operations interface
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Add two objects of numeric type.
+ *
+ * @return
+ *   new numeric object containing sum
+ */
+extern DfObject* df_num_add(DfObject *a, DfObject *b);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DF_OBJECTOPS_H_ */

commit b6f5f7c7d2cb5da661df95b0f5ac3129fd4c93c3

object: ops: fix pointer type
Fixes: 2653aae51867 ("object: add abstract object operations interface")
Cc: dilshod@difengine.xyz

difengine/objectops.c
@@ -8,5 +8,5 @@ df_num_add(DfObject *a, DfObject *b)
     DfIntObj *sum = df_int_obj_new(((DfIntObj *)a)->val +
                                    ((DfIntObj *)b)->val);
 
-    return sum;
+    return (DfObject *)sum;
 }