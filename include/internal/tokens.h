urazoff/difengine

commit c4953f7d740f9bf38032fd895d1161a8e08fdf3c

front-end: add initial token defines

include/internal/tokens.h
@@ -1,0 +1,48 @@
+#ifndef _DF_TOKENS_H_
+#define _DF_TOKENS_H_
+
+#include "difengine.h"
+
+/**
+ * @file
+ * Difengine token types
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    /* character tokens */
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    ASTERISK,
+    GREATER,
+    LESS,
+    EQUAL,
+    GREATEREQ,
+    LESSEQ,
+    EQUALEQ,
+    NOTEQUAL,
+    /* literals */
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+    /* utility tokens */
+    ERROR,
+    ENDOF
+} DfTokens;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DF_TOKENS_H_ */

commit 71d893d3faf1bb3fb359cd60e5522aa50b59b5f9

front-end: add prefix to error and EOF tokens
This is needed to prevent future confusion.

Fixes: c4953f7d740f ("front-end: add initial token defines")
Cc: dilshod@difengine.xyz

include/internal/tokens.h
@@ -37,8 +37,8 @@ typedef enum {
     STRING,
     NUMBER,
     /* utility tokens */
-    ERROR,
-    ENDOF
+    TERROR,
+    TENDOF
 } DfTokens;
 
 #ifdef __cplusplus