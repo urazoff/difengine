urazoff/difengine

commit 9921272fea2114a8f4e3258adf7561c12caa5831

front-end: add lexers primitives

front/lexer.c
@@ -1,0 +1,55 @@
+#include "internal/tokens.h"
+
+/**
+ * Recognize single character token.
+ *
+ * @return
+ *   token type
+ */
+static int df_token_single(char c)
+{
+    switch (c)
+    {
+        case '(': return LPAREN;
+        case ')': return RPAREN;
+        case '{': return LBRACE;
+        case '}': return RBRACE;
+        case ',': return COMMA;
+        case '.': return DOT;
+        case '-': return MINUS;
+        case '+': return PLUS;
+        case ';': return SEMICOLON;
+        case '/': return SLASH;
+        case '*': return ASTERISK;
+        case '>': return GREATER;
+        case '<': return LESS;
+        case '=': return EQUAL;
+    }
+
+    return TERROR;
+}
+
+/**
+ * Recognize two-character token.
+ *
+ * @return
+ *   token type
+ */
+static int df_token_two(char c1, char c2)
+{
+#define WITH_EQUAL(_token) \
+    do {                              \
+        if (c2 == '=') return _token; \
+        break;                        \
+    } while (0)
+
+    switch(c1)
+    {
+        case '>': WITH_EQUAL(GREATEREQ);
+        case '<': WITH_EQUAL(LESSEQ);
+        case '=': WITH_EQUAL(EQUALEQ);
+        case '!': WITH_EQUAL(NOTEQUAL);
+    }
+
+    return TERROR;
+}

front/README
@@ -1,0 +1,1 @@
+Front-end (lexer aka tokenizer aka lexical analyzer, parser etc).

front/meson.build
@@ -1,0 +1,3 @@
+front_lib = static_library('front',
+                           ['lexer.c'],
+                           include_directories: global_inc)

meson.build
@@ -4,5 +4,6 @@ cc = meson.get_compiler('c')
 
 global_inc = include_directories('include')
 
+subdir('front')
 subdir('difengine')
 subdir('tests')

commit 4db319770f6eb84add1e3ce2d736dad29ba462de

style: fix function return type and name layout in lexer
Fixes: 9921272fea21 ("front-end: add lexers primitives")
Cc: dilshod@difengine.xyz

front/lexer.c
@@ -6,7 +6,8 @@
  * @return
  *   token type
  */
-static int df_token_single(char c)
+static int
+df_token_single(char c)
 {
     switch (c)
     {
@@ -35,7 +36,8 @@ static int df_token_single(char c)
  * @return
  *   token type
  */
-static int df_token_two(char c1, char c2)
+static int
+df_token_two(char c1, char c2)
 {
 #define WITH_EQUAL(_token) \
     do {                              \