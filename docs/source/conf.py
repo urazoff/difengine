urazoff/difengine

commit 85dea0ed57c18c30598be5789ddbb58c4ae52cca

docs: generate doxygen documentation

docs/source/conf.py
@@ -54,3 +54,14 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 master_doc = 'index'
+
+# -- Doxygen -----------------------------------------------------------------
+
+import subprocess
+
+doxyfile = 'api/Doxyfile'
+cmd = 'cd ..; doxygen ' + doxyfile
+
+subprocess.call(cmd, shell=True)
+
+html_extra_path = ['../docs/api/gendocs/html']

commit b7935444b42f8f18327e0e00a9439f2eed005d1a

docs: fix conf.py doxygen section
Fixes: 85dea0ed57c1 ("docs: generate doxygen documentation")
Cc: dilshod@difengine.xyz

docs/source/conf.py
@@ -59,9 +59,9 @@ master_doc = 'index'
 
 import subprocess
 
-doxyfile = 'api/Doxyfile'
-cmd = 'cd ..; doxygen ' + doxyfile
+doxyfile = 'docs/api/Doxyfile'
+cmd = 'cd ../..; doxygen ' + doxyfile
 
 subprocess.call(cmd, shell=True)
 
-html_extra_path = ['../docs/api/gendocs/html']
+html_extra_path = ['docs/api/gendocs/html']