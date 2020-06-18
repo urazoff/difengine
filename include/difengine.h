urazoff/difengine

commit 79c6b223065f7f68190dbd3c9b318af8728158d2

common: add meta-include header

include/difengine.h
@@ -1,0 +1,13 @@
+#ifndef _DF_DEFINES_H_
+#define _DF_DEFINES_H_
+
+/**
+ * @file
+ * Meta-include header file
+ */
+
+#include <stdbool.h> 
+#include <stddef.h>  
+#include <stdint.h>  
+
+#endif /* DF_DIFENGINE_H_ */

commit 238335a70566e8aae48c8f84ae8c18b24a2e2aaa

common: fix meta-include header
The following typo breaks defines.h linking.

Fixes: 79c6b223065f ("common: add meta-include header")
Cc: dilshod@difengine.xyz

include/difengine.h
@@ -1,5 +1,5 @@
-#ifndef _DF_DEFINES_H_
-#define _DF_DEFINES_H_
+#ifndef _DF_DIFENGINE_H_
+#define _DF_DIFENGINE_H_
 
 /**
  * @file