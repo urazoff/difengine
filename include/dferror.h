405ba23db5ab2d91aae43b57f4dbe1601ce52c21
Dilshod Urazov <dilshod@difengine.xyz>
2020-07-02 12:12:15 +0300

    common: add special error codes.

---
+++ include/dferror.h
@@ -0,0 +1,19 @@
+#ifndef _DF_ERROR_H_
+#define _DF_ERROR_H_
+
+/**
+ * @file
+ * Special error codes.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define ETOKEN    10 /* Invalid token */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DF_ERROR_H_ */
