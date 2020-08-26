urazoff/difengine

commit dba1a24f3fbefbb51f25ee47ada09f80407ee67a

object: hash: implement hash table

include/hashobject.h
@@ -1,0 +1,70 @@
+#ifndef _DF_LISTOBJECT_H_
+#define _DF_LISTOBJECT_H_
+
+#include "difengine.h"
+#include "object.h"
+
+/**
+ * @file
+ * Hash table
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    uint32_t hash;
+    DfObject *key;
+    DfObject *value;
+} DfHashEntry;
+
+typedef struct {
+    DfObject obj;
+    unsigned int size;
+    int iprime;
+    unsigned int count;
+    unsigned int deleted;
+    DfHashEntry *entries;
+} DfHashObj; 
+
+extern DfType DfHashType;
+
+/**
+ * Allocate new hash object and initialize it.
+ *
+ * @return
+ *   new & empty hash object
+ */
+extern DfObject *df_hash_obj_init();
+
+/**
+ * Find value by key.
+ *
+ * @return
+ *   found value by key or NULL
+ */
+extern DfObject *df_hash_obj_get(DfObject *ht, DfObject *key);
+
+/**
+ * Insert new key-value pair.
+ *
+ * @return
+ *   inserted value or NULL
+ */
+extern DfObject *df_hash_obj_insert(DfObject *ht, DfObject *key,
+                                    DfObject *value);
+
+/**
+ * Delete entry by key.
+ *
+ * @return
+ *   0 on successful search and deletion, -1 otherwise 
+ */
+extern int df_hash_obj_delete(DfObject *ht, DfObject *key);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DF_LISTOBJECT_H_ */

difengine/hashobject.c
@@ -1,0 +1,327 @@
+#include "hashobject.h"
+#include "object.h"
+#include "memory.h"
+#include "stringobject.h"
+
+static const struct {
+    unsigned int maxload;
+    unsigned int size;
+} hash_primes[]= {
+        { 2,            5},
+        { 4,            7},
+        { 8,            13},
+        { 16,           19},
+        { 32,           43},
+        { 64,           73},
+        { 128,          151},
+        { 256,          283},
+        { 512,          571},
+        { 1024,         1153},
+        { 2048,         2269},
+        { 4096,         4519},
+        { 8192,         9013},
+        { 16384,        18043},
+        { 32768,        36109},
+        { 65536,        72091},
+        { 131072,       144409},
+        { 262144,       288361},
+        { 524288,       576883},
+        { 1048576,      1153459},
+        { 2097152,      2307163},
+        { 4194304,      4613893},
+        { 8388608,      9227641},
+        { 16777216,     18455029},
+        { 33554432,     36911011},
+        { 67108864,     73819861},
+        { 134217728,    147639589},
+        { 268435456,    295279081},
+        { 536870912,    590559793},
+        { 1073741824,   1181116273},
+};
+
+static const unsigned int prime_length = sizeof(hash_primes)/sizeof(hash_primes[0]);
+
+/* An object to mark entry as deleted */
+static DfObject *bogus;
+
+static int
+is_free(DfHashEntry *e)
+{
+    return e->key == NULL;
+}
+
+static int
+is_deleted(DfHashEntry *e)
+{
+    return e->key == bogus;
+}
+
+static int
+is_present(DfHashEntry *e)
+{
+    return e->key != NULL && e->key != bogus;
+}
+
+DfObject*
+df_hash_obj_init()
+{
+    DfHashObj *ht = (DfHashObj *)df_obj_new(&DfHashType);
+
+    /* Initialize bogus key */
+    if (bogus == NULL)
+    {
+        bogus = (DfObject *)df_str_obj_from_str("bogus");
+        if (bogus == NULL)
+            return NULL;
+    }
+
+    if (ht == NULL)
+        return NULL;
+
+    ht->iprime = 0;
+    ht->size = hash_primes[ht->iprime].size;
+    ht->count = 0;
+    ht->deleted = 0;
+    ht->entries = (DfHashEntry *)DF_MEM_ALLOC(ht->size * sizeof(DfHashEntry));
+
+    return (DfObject *)ht;
+}
+
+/* One search routine to rule them all */
+static DfHashEntry*
+hash_search(DfHashObj *ht, DfObject *key, uint32_t hash)
+{
+    DfHashEntry *e;
+    DfHashEntry *available = NULL;
+    uint32_t i;
+    uint32_t start;
+
+    start = hash % ht->size;
+    i = start;
+
+    do {
+        e = ht->entries + i;
+
+        if (is_present(e) == 0)
+        {
+             if (available == NULL)
+                 available = e;
+             if (is_free(e))
+                 break;
+        }
+
+        if (is_deleted(e) == 0 && e->hash == hash &&
+            df_obj_compare(e->key, key) == 0)
+            return e;
+
+        i = (i + hash % (ht->size - 2) + 1) % ht->size;
+    } while(i != start);
+
+    if (available != NULL)
+        return available;
+
+    return NULL;
+}
+
+static DfHashEntry*
+hash_insert(DfHashObj *ht, DfObject *key, DfObject *value, uint32_t hash);
+
+/** Expand hash table.
+ *  NB: this function rearranges entries.
+ */
+static DfHashObj*
+hash_rehash(DfHashObj *ht, int newiprime)
+{
+    DfHashEntry *table;
+    DfHashEntry *e;
+    DfHashEntry *oldtable = ht->entries;
+    unsigned int newsize;
+    unsigned int oldsize;
+    int i;
+
+    if (newiprime >= prime_length)
+        return NULL;
+
+    oldsize = ht->size;
+    newsize = hash_primes[newiprime].size;
+    table = (DfHashEntry *)DF_MEM_ALLOC(newsize * sizeof(DfHashEntry));
+    if (table == NULL)
+        return NULL;
+
+    memset(table, 0, newsize * sizeof(DfHashEntry));
+    ht->entries = table;
+    ht->size = newsize;
+    ht->iprime = newiprime;
+    ht->count = 0;
+
+    for (i = 0, e = oldtable; i < oldsize; i++, e++)
+    {
+        if (is_present(e))
+            hash_insert(ht, e->key, e->value, e->hash);
+    }
+
+    return ht;
+}
+
+static DfHashEntry*
+hash_insert(DfHashObj *ht, DfObject *key, DfObject *value, uint32_t hash)
+{
+    DfHashEntry *e;
+
+    if (ht->count >= hash_primes[ht->iprime].maxload)
+        hash_rehash(ht, ht->iprime + 1);
+
+    e = hash_search(ht, key, hash);
+
+    if (is_present(e))
+    {
+        DF_DEC_RF(e->key);
+        DF_DEC_RF(e->value);
+        e->value = value;
+        e->key = key;
+    }
+    else
+    {
+        if (is_free(e) == 0)
+             DF_DEC_RF(e->key);
+        e->hash = hash;
+        e->key = key;
+        e->value = value;
+        ht->count++;
+    }
+
+    return e;
+}
+
+/* Delete found entry */
+static void
+hash_delete_entry(DfHashObj *ht, DfHashEntry *e)
+{
+    /* check if entry either free or deleted*/
+    if (e->key == NULL || e->key == bogus)
+        return;
+
+    e->key = bogus;
+    ht->count--;
+    ht->deleted--;
+
+    DF_INC_RF(bogus);
+    DF_DEC_RF(e->key);
+    DF_DEC_RF(e->value);
+
+    e->value = NULL;
+}
+
+DfObject*
+df_hash_obj_get(DfObject *ht, DfObject *key)
+{
+    uint32_t hash;
+
+    if (ht->type != &DfHashType)
+        return NULL;
+
+    if (((DfHashObj *)ht)->entries == NULL)
+        return NULL;
+
+    hash = df_obj_hash(key);
+    /* Not hashable key */
+    if (hash == 0)
+       return NULL;
+
+    return hash_search((DfHashObj *)ht, key, hash)->value;
+}
+
+DfObject*
+df_hash_obj_insert(DfObject *ht, DfObject *key, DfObject *value)
+{
+    uint32_t hash;
+
+    if (((DfObject *)ht)->type != &DfHashType)
+        return NULL;
+
+    hash = df_obj_hash(key);
+    if (hash == 0)
+       return NULL;
+
+    DF_INC_RF(key);
+    DF_INC_RF(value);
+    return hash_insert((DfHashObj *)ht, key, value, hash)->value;
+}
+
+int
+df_hash_obj_delete(DfObject *ht, DfObject *key)
+{
+    DfHashEntry *e;
+    uint32_t hash;
+
+    if (ht->type != &DfHashType)
+        return -1;
+
+    if (((DfHashObj *)ht)->entries == NULL)
+        return -1;
+
+    hash = df_obj_hash(key);
+    if (hash == 0)
+        return -1;
+
+    e = hash_search((DfHashObj *)ht, key, hash);
+    if (e == NULL)
+        return -1;
+
+    hash_delete_entry((DfHashObj *)ht, e);
+
+    return 0;
+}
+
+/* How many entries with no bogus neither NULL keys? */
+static int
+hash_length(DfHashObj *ht)
+{
+    return ht->count;
+}
+
+static void
+hash_destroy(DfObject *ht)
+{
+    int i;
+    DfHashEntry *e;
+    DfHashObj *htp = (DfHashObj *)ht;
+
+    for (i = 0, e = htp->entries; i < htp->size; i++, e++)
+    {
+        if (is_present(e))
+        {
+            DF_DEC_RF(e->key);
+            DF_DEC_RF(e->value);
+        }
+    }
+
+    DF_MEM_FREE(htp->entries);
+    DF_MEM_FREE(htp);
+}
+
+static DfContOps as_container = {
+    (intunaryop)hash_length,
+    NULL
+};
+
+static DfAssContOps as_associative = {
+    (binaryop)df_hash_obj_get,
+    (intbinaryop)df_hash_obj_delete,
+    (ternaryop)df_hash_obj_insert
+};
+
+DfType DfHashType = {
+    "hash table",
+    sizeof(DfHashObj),
+    0,
+    (voidunaryop)hash_destroy,
+    NULL,
+    NULL, /* TODO: (intunaryop)hash_print */
+    NULL, /* TODO: (intbinaryop)hash_compare */
+    NULL, /* TODO: (getter)hash_get */
+    NULL,
+    NULL,
+    &as_container,
+    &as_associative
+};

difengine/meson.build
@@ -12,6 +12,11 @@ str_lib = static_library('string',
                           include_directories: global_inc,
                           link_with: [mem_lib, obj_lib])
 
+hash_lib = static_library('hash',
+                          'hashobject.c',
+                           include_directories: global_inc,
+                           link_with: [mem_lib, obj_lib, str_lib])
+
 code_lib = static_library('code',
                           'code.c',
                            include_directories: global_inc,
@@ -55,6 +60,7 @@ df_libs += [
             mem_lib,
             obj_lib,
             str_lib,
+            hash_lib,
             code_lib,
             frame_lib,
             int_lib,

tests/api/hash.c
@@ -1,0 +1,35 @@
+#include <stdio.h>
+#include "defines.h"
+#include "hashobject.h"
+#include "stringobject.h"
+
+int main(int argc, char *argv[])
+{
+    int ret_val;
+    DfObject *ret_p;
+    DfStrObj *str1 = df_str_obj_from_str("string #1");
+    DfStrObj *str2 = df_str_obj_from_str("string #2");
+    DfObject *ht = df_hash_obj_init();
+
+    ret_val = df_obj_print((DfObject *)str1);
+    printf("%u\n", str1->hash);
+
+    ret_val = df_obj_print((DfObject *)str2);
+    printf("%u\n", str2->hash);
+
+    ret_p = df_hash_obj_insert(ht, (DfObject *)str1, (DfObject *)str2);
+    ret_val = df_obj_print(ret_p);
+
+    ret_p = df_hash_obj_get(ht, (DfObject *)str1);
+    ret_val = df_obj_print(ret_p);
+
+    ret_val = df_hash_obj_delete(ht, (DfObject *)str1);
+    ret_p = df_hash_obj_get(ht, (DfObject *)str1);
+    ret_val = df_obj_print(ret_p);
+
+    UNUSED(ret_val);
+
+    printf("RES: Success\n");
+
+    return 0;
+}

tests/api/meson.build
@@ -4,7 +4,8 @@ tests = [['code', ['code.c']],
          ['frame', ['frame.c']],
          ['evaluate', ['evaluate.c']],
          ['lexer', ['lexer.c']],
-         ['parser', ['parser.c']]]
+         ['parser', ['parser.c']],
+         ['hash', ['hash.c']]]
 
 foreach t:tests
     exe = executable(t[0], t[1], include_directories: global_inc,

commit 949a50508bd119bc610345f1f6ea87ce531209d7

object: hash: fix header
Fixes: dba1a24f3fbe ("object: hash: implement hash table")
Cc: dilshod@difengine.xyz

include/hashobject.h
@@ -1,5 +1,5 @@
-#ifndef _DF_LISTOBJECT_H_
-#define _DF_LISTOBJECT_H_
+#ifndef _DF_HASHOBJECT_H_
+#define _DF_HASHOBJECT_H_
 
 #include "difengine.h"
 #include "object.h"
@@ -67,4 +67,4 @@ extern int df_hash_obj_delete(DfObject *ht, DfObject *key);
 }
 #endif
 
-#endif /* DF_LISTOBJECT_H_ */
+#endif /* DF_HASHOBJECT_H_ */