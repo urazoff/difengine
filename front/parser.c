urazoff/difengine

commit 6866176daf3215ab7c80505300a7a89488aa9c52

front-end: parser: support identifier assignment

front/parser.c
@@ -88,7 +88,7 @@ DfParseRule parse_rules[] = {
   { NULL, binary_rule, P_FACTOR, N_MULTIPLY},
   { NULL, NULL, P_NONE, -1},
   { NULL, NULL, P_NONE, -1},
-  { NULL, NULL, P_NONE, -1},
+  { unary_rule, binary_rule, P_ASSIGNMENT, N_EQUAL},
   { NULL, NULL, P_NONE, -1},
   { NULL, NULL, P_NONE, -1},
   { NULL, NULL, P_NONE, -1},

tests/api/parser.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     DfTree *x = NULL;
-    const char *inp_str = "(-12 + 2)*val - 31/2 + \"str\"";
+    const char *inp_str = "val = (-12 + 2)*val - 31/2 + \"str\"";
 
     printf("expr: %s;\n", inp_str);
     DfLexer *lexer = df_lexer_init_from_str(inp_str);

front/ast.c
@@ -8,6 +8,7 @@ const char * const NodeTypes[] = {
     "MULTIPLY",
     "GREATER",
     "LESS",
+    "EQUAL",
     "GREATEREQ",
     "LESSEQ",
     "EQUALEQ",

include/ast.h
@@ -19,6 +19,7 @@ typedef enum {
     N_MULTIPLY,
     N_GREATER,
     N_LESS,
+    N_EQUAL,
     N_GREATEREQ,
     N_LESSEQ,
     N_EQUALEQ,

commit 0e42693f3961d8bd436d05adc7869d33646ba46f

front-end: parser: fix assignment
Fixes: 6866176daf32 ("front-end: parser: support identifier assignment")
Cc: dilshod@difengine.com

front/parser.c
@@ -88,7 +88,7 @@ DfParseRule parse_rules[] = {
   { NULL, binary_rule, P_FACTOR, N_MULTIPLY},
   { NULL, NULL, P_NONE, -1},
   { NULL, NULL, P_NONE, -1},
-  { unary_rule, binary_rule, P_ASSIGNMENT, N_EQUAL},
+  { NULL, binary_rule, P_ASSIGNMENT, N_EQUAL},
   { NULL, NULL, P_NONE, -1},
   { NULL, NULL, P_NONE, -1},
   { NULL, NULL, P_NONE, -1},
@@ -203,7 +203,7 @@ grouping_rule(DfParser *parser)
 {
     DfTree *x = NULL;
 
-    x = df_parser_precedence(parser, P_ASSIGNMENT);
+    x = df_parser_precedence(parser, P_OR);
     df_parser_expect(parser, RPAREN, "')' after expression");
 
     return x;