urazoff/difengine

commit d3936c7d04706256be1d6944a916501e3ac1e08a

object: list: add list object type skeleton

difengine/listobject.c
@@ -2,37 +2,43 @@
 #include "listobject.h"
 #include "memory.h"
 
-DfListObj*
+DfObject*
 df_list_obj_init()
 {
-    DfListObj *list = DF_MEM_ALLOC(sizeof(DfListObj));
+    DfListObj *list = (DfListObj *)df_obj_new(&DfListType);
+
+    if (list == NULL)
+        return NULL;
 
     list->items = NULL;
     list->count= 0;
     list->capacity = 0;
     list->overallocate = 0;
 
-    return list;
+    return (DfObject *)list;
 }
 
 void
-df_list_obj_extend(DfListObj *list, DfObject *item)
+df_list_obj_extend(DfObject *list, DfObject *item)
 {
-    if (list->capacity < list->count + 1)
+    DfListObj *listp = (DfListObj *)list;
+
+    if (listp->capacity < listp->count + 1)
     {
-        int old_capacity = list->capacity;
+        int old_capacity = listp->capacity;
 
-        if (list->overallocate != 0)
-            list->capacity = CONT_UPD_CAPACITY(old_capacity);
+        if (listp->overallocate != 0)
+            listp->capacity = CONT_UPD_CAPACITY(old_capacity);
         else
-            list->capacity++;
+            listp->capacity++;
 
-        list->items = DF_MEM_GROW_ARRAY(DfObject *, list->items,
-                                        old_capacity, list->capacity);
+        listp->items = DF_MEM_GROW_ARRAY(DfObject *, listp->items,
+                                         old_capacity, listp->capacity);
     }
 
-    list->items[list->count] = item;
-    list->count++;
+    listp->items[listp->count] = item;
+    DF_INC_RF(item);
+    listp->count++;
 }
 
 void
@@ -42,5 +48,70 @@ df_list_obj_clear(DfListObj *list)
 
     DF_MEM_FREE(list);
 
-    list = df_list_obj_init();
+    list = (DfListObj *)df_list_obj_init();
 }
+
+static int
+list_print(DfObject *list)
+{
+    int i;
+    DfListObj *listp = (DfListObj *)list;
+
+    printf("[");
+    if (listp->items != NULL)
+    {
+        for (i = 0; i < listp->count; i++)
+        {
+             if (i > 0)
+                 printf(", ");
+
+             if (df_obj_print(listp->items[i], 0) != 0)
+                return -1;
+        }
+    }
+    printf("]");
+
+    return 0;
+}
+
+static int
+list_length(DfObject *list)
+{
+    return ((DfListObj *)list)->count;
+}
+
+static void
+list_destroy(DfObject *list)
+{
+    int i;
+    DfListObj *listp = (DfListObj *)list;
+
+    if (listp->items != NULL)
+    {
+        for (i = 0; i < listp->count; i++)
+            DF_DEC_RF(listp->items[i]);
+
+        DF_MEM_FREE_ARRAY(DfObject *, listp->items, listp->capacity);
+    }
+    DF_MEM_FREE(listp);
+}
+
+static DfContOps as_container = {
+    (intunaryop)list_length,
+    NULL
+};
+
+DfType DfListType = {
+    "list",
+    sizeof(DfListObj),
+    0,
+    (voidunaryop)list_destroy,
+    NULL,
+    (intunaryop)list_print,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    &as_container,
+    NULL
+};

difengine/code.c
@@ -11,7 +11,7 @@ df_code_obj_init(int stack_size)
     DfCodeObj *code = DF_MEM_ALLOC(sizeof(DfCodeObj));
 
     code->opcodes = df_bytes_obj_init();
-    code->consts = df_list_obj_init();
+    code->consts = (DfListObj *)df_list_obj_init();
     code->lines = df_bytes_obj_init();
     code->stack_size = stack_size;
 
@@ -89,7 +89,7 @@ df_code_obj_add_op(DfCodeObj *code, uint8_t opcode, int line)
 int
 df_code_obj_add_const(DfCodeObj *code, DfObject *const_obj)
 {
-    df_list_obj_extend(code->consts, const_obj);
+    df_list_obj_extend((DfObject *)code->consts, const_obj);
 
     return code->consts->count - 1;
 }

include/listobject.h
@@ -18,18 +18,20 @@ typedef struct {
     DfObject **items;
 } DfListObj;    
 
+extern DfType DfListType;
+
 /**
  * Allocate new list object and initialize it.
  *
  * @return
  *   new&empty list object
  */
-extern DfListObj *df_list_obj_init();
+extern DfObject *df_list_obj_init();
 
 /**
  * Extend the list by adding new item.
  */
-extern void df_list_obj_extend(DfListObj *list, DfObject *item);
+extern void df_list_obj_extend(DfObject *list, DfObject *item);
 
 /**
  * Clear out the list.

tests/api/list.c
@@ -1,50 +1,32 @@
 #include <stdio.h>
 #include "difengine.h"
+#include "defines.h"
 #include "memory.h"
 #include "object.h"
-#include "intobject.h"
-#include "floatobject.h"
+#include "hashobject.h"
+#include "stringobject.h"
 #include "listobject.h"
 
 int main(int argc, char *argv[])
 {
-    DfListObj *list;
-    DfListObj *get_list;
-    DfObject *object = NULL;
-    DfIntObj *num = DF_MEM_ALLOC(sizeof(DfIntObj));
-    DfIntObj *get_num;
-    DfFloatObj *f_num =  DF_MEM_ALLOC(sizeof(DfFloatObj));
-    DfFloatObj *get_f_num;
+    int ret_val;
+    DfObject *ret_p;
+    DfStrObj *str1 = df_str_obj_from_str("string #1");
+    DfStrObj *str2 = df_str_obj_from_str("string #2");
+    DfObject *ht = df_hash_obj_init();
+    DfObject *list = df_list_obj_init();
 
-    num->val = 1923;
-    f_num->val = 3.1415;
-    list = df_list_obj_init();
-    list->overallocate = true;
+    df_list_obj_extend(list, (DfObject *)str1);
+    df_list_obj_extend(list, (DfObject *)str2);
 
-    df_list_obj_extend(list, object);
-    df_list_obj_extend(list, (DfObject *)num);
-    df_list_obj_extend(list, (DfObject *)f_num);
-    /* Put the list as an element of itself */
-    df_list_obj_extend(list, (DfObject *)list);
+    ret_p = df_hash_obj_insert(ht, (DfObject *)str1, (DfObject *)str2);
 
-    printf("Code:\n---- count: %u \n"
-           "---- capacity %u \n",
-           list->count,
-           list->capacity);
+    df_list_obj_extend(list, ht);
 
-    get_num = (DfIntObj *)list->items[list->count - 3];
-    printf("Integer:\n ---- n=%d\n", get_num->val);
+    ret_val = df_obj_print(list, 1);
 
-    get_f_num = (DfFloatObj *)list->items[list->count - 2]; 
-    printf("Float:\n ---- n=%f\n", get_f_num->val);
-
-    get_list = (DfListObj *)list->items[list->count - 1];
-    get_num = (DfIntObj *)get_list->items[get_list->count - 3]; 
-    printf("Integer again:\n ---- n=%d\n", get_num->val);
-
-    DF_MEM_FREE(num);
-    DF_MEM_FREE(f_num);
-    df_list_obj_clear(list);
+    UNUSED(ret_val);
+    UNUSED(ret_p);
 
     printf("RES: Success\n");
 

commit 2bd4ca005c5f27e510e2ef1a5476693c321a5a92

object: list: fix item size in type
Fixes: d3936c7d0470 ("object: list: add list object type skeleton")
Cc: dilshod@difengine.com

difengine/listobject.c
@@ -104,7 +104,7 @@ static DfContOps as_container = {
 DfType DfListType = {
     "list",
     sizeof(DfListObj),
-    0,
+    sizeof(DfObject *),
     (voidunaryop)list_destroy,
     NULL,
     (intunaryop)list_print,