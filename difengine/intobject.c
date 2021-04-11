urazoff/difengine

commit a39dbe204bf8e3c592b1599daca6d16c70b5b340

object: int: add integer type
Int type supports arbitrary-precision arithmetic.

difengine/intobject.c
@@ -1,12 +1,541 @@
 #include "intobject.h"
+#include "defines.h"
 #include "memory.h"
 
-DfIntObj*
-df_int_obj_new(int val)
+DfObject*
+df_int_obj_init(int size)
 {
-    DfIntObj *int_obj = DF_MEM_ALLOC(sizeof(DfIntObj));
+    DfIntObj *x = (DfIntObj *)df_obj_new(&DfIntType);
 
-    int_obj->val = val;
+    if (x == NULL)
+        return NULL;
 
-    return int_obj;
+    x->digits = DF_MEM_ALLOC(size * sizeof(df_digit));
+    if (x->digits == NULL)
+        return NULL;
+    memset(x->digits, 0, size * sizeof(df_digit));
+    x->count = size;
+    x->capacity = size;
+    x->overallocate = 0;
+
+    return (DfObject *)x;
 }
+
+void
+df_int_print_digits(DfObject *a)
+{
+    DfIntObj *x = (DfIntObj *)a;
+    int i;
+    int size = DF_ABS(x->count);
+
+    if (x->count < 0)
+        printf("-");
+    printf("[");
+    for (i = 0; i < size; ++i)
+    {
+             if (i > 0)
+                 printf(", ");
+
+             printf("%d", x->digits[i]);
+    }
+    printf("]");
+}
+
+static DfIntObj*
+remove_leading_zeros(DfIntObj *a)
+{
+    int i;
+    int size = DF_ABS(a->count);
+
+    for (i = size; (i > 0) && (a->digits[i - 1] == 0); --i);
+
+    if (i != size)
+        a->count = (a->count >= 0) ? i : -i;
+
+    return a;
+}
+
+static DfObject*
+int_negate(DfObject *a)
+{
+    DfIntObj *x = (DfIntObj *)a;
+    int size_x = DF_ABS(x->count);
+    int i;
+
+    DfIntObj *neg = (DfIntObj *)df_int_obj_init(size_x);
+    for (i = 0; i < size_x; ++i)
+        neg->digits[i] = x->digits[i];
+    neg->count = -size_x;
+
+    return (DfObject *)neg;
+}
+
+/* GNU style compare function for absolute values.
+ * Return -1 if |x| < |y|, 0 if |x| == |y|, 1 if |x| > |y|.
+ * */
+static int
+int_compare_abs(DfIntObj *x, DfIntObj *y)
+{
+    int size_x = DF_ABS(x->count);
+    int size_y = DF_ABS(y->count);
+    int i;
+
+    /* The more significant figures the bigger absolute value */
+    if (size_x != size_y)
+        return size_x > size_y ? 1 : -1;
+
+    /* Compare per digit starting from the most significant one */
+    for (i = size_x; i-- > 0;)
+    {
+       if (x->digits[i] != y->digits[i])
+           return x->digits[i] > y->digits[i] ? 1 : -1;
+    }
+
+    return 0;
+}
+
+/* Primary school method ~ O(n).
+ * Doesn't handle signs. Assume |x->count| >= |y->count|.
+ * */
+static DfIntObj*
+int_add_s(DfIntObj *x, DfIntObj *y)
+{
+    df_digit carry = 0;
+    int i;
+    int size_x;
+    int size_y;
+
+    size_x = DF_ABS(x->count);
+    size_y = DF_ABS(y->count);
+
+    DfIntObj *s = (DfIntObj *)df_int_obj_init(size_x + 1);
+    if (s == NULL)
+        return NULL;
+
+    for (i = 0; i < size_y; ++i)
+    {
+        carry += x->digits[i] + y->digits[i];
+        s->digits[i] = carry % DF_INT_BASE;
+        /* carry = carry / DF_INT_BASE via bitwise shift */
+        carry >>= DF_INT_BITS;
+    }
+
+    for (; i < size_x; ++i)
+    {
+        carry += x->digits[i];
+        s->digits[i] = carry % DF_INT_BASE;
+        carry >>= DF_INT_BITS;
+    }
+
+    s->digits[size_x] = carry;
+    return remove_leading_zeros(s);
+}
+
+/* Substract absolute values. Assume |x| > |y|. */
+static DfIntObj*
+int_sub_s(DfIntObj *x, DfIntObj *y)
+{
+    df_digit carry = 0;
+    int i;
+    int size_x;
+    int size_y;
+
+    size_x = DF_ABS(x->count);
+    size_y = DF_ABS(y->count);
+
+    DfIntObj *s = (DfIntObj *)df_int_obj_init(size_x);
+    if (s == NULL)
+        return NULL;
+
+    for (i = 0; i < size_y; ++i)
+    {
+        s->digits[i] = x->digits[i] - y->digits[i] - carry;
+        carry = s->digits[i] >> (DF_INT_DIGIT_SIZE - 1u);
+        s->digits[i] &= DF_INT_MASK;
+    }
+
+    for (; i < size_x; ++i)
+    {
+        s->digits[i] = x->digits[i] - carry;
+        carry = s->digits[i] >> (DF_INT_DIGIT_SIZE - 1u);
+        s->digits[i] &= DF_INT_MASK;
+    }
+
+    return remove_leading_zeros(s);
+}
+
+/* Addition wrapper. Handles signs. */
+static DfObject*
+int_add(DfObject *a, DfObject *b)
+{
+    DfIntObj *x = (DfIntObj *)a;
+    DfIntObj *y = (DfIntObj *)b;
+    DfIntObj *z;
+
+    if (DF_SGN(x->count) == DF_SGN(y->count))
+    {
+        /* Make sure |x->count| >= |y->count| */
+        if (DF_ABS(x->count) < DF_ABS(y->count))
+        {
+            x = (DfIntObj *)b;
+            y = (DfIntObj *)a;
+        }
+        z = int_add_s(x, y);
+    }
+    else
+    {
+        /* Make sure |x| >= |y| */
+        if (int_compare_abs(x, y) < 0)
+        {
+            x = (DfIntObj *)b;
+            y = (DfIntObj *)a;
+        }
+        z = int_sub_s(x, y);
+    }
+
+    z->count = x->count < 0 ? -z->count : z->count;
+    return (DfObject *)z;
+}
+
+/* Substraction wrapper. Handles signs. */
+static DfObject*
+int_sub(DfObject *a, DfObject *b)
+{
+    DfIntObj *x = (DfIntObj *)a;
+    DfIntObj *y = (DfIntObj *)b;
+    DfIntObj *z;
+    int sign = 0;
+
+    if (DF_SGN(x->count) != DF_SGN(y->count))
+    {
+        /* Make sure |x->count| >= |y->count| */
+        if (DF_ABS(x->count) < DF_ABS(y->count))
+        {
+            x = (DfIntObj *)b;
+            y = (DfIntObj *)a;
+        }
+        z = int_add_s(x, y);
+        z->count = x->count < 0 ? -z->count : z->count;
+    }
+    else
+    {
+        sign = x->count < 0 ? -1 : 1;
+        /* Make sure |x| >= |y| */
+        if (int_compare_abs(x, y) < 0)
+        {
+            sign = -sign;
+            x = (DfIntObj *)b;
+            y = (DfIntObj *)a;
+        }
+        z = int_sub_s(x, y);
+        z->count = sign*z->count;
+    }
+
+    return (DfObject *)z;
+}
+
+/* Primary school method ~ O(n^2). */
+static DfIntObj*
+int_multiply_s(DfIntObj *x, DfIntObj *y)
+{
+    df_word carry;
+    df_digit c;
+    int ix;
+    int iy;
+    int size;
+    int size_x;
+    int size_y;
+    int d;
+
+    size_x = DF_ABS(x->count);
+    size_y = DF_ABS(y->count);
+    size = size_x + size_y;
+
+    DfIntObj *p = (DfIntObj *)df_int_obj_init(size);
+    if (p == NULL)
+        return NULL;
+
+    for (ix = 0; ix < size_x; ++ix)
+    {
+        c = 0;
+        d = size - ix;
+        for (iy = 0; iy < d; ++iy)
+        {
+            carry = (df_word)p->digits[ix + iy] +
+                    ((df_word)x->digits[ix] * (df_word)y->digits[iy]) +
+                    (df_word)c;
+            p->digits[ix + iy] = (df_digit)(carry & DF_INT_MASK);
+            c = (df_digit)(carry >> DF_INT_BITS);
+        }
+    }
+    p->digits[size - 1] = c;
+
+    return remove_leading_zeros(p);
+}
+
+/* Multiplication wrapper.
+ * TODO: implement Karatsuba and Toom-3 algorithms and add
+ * to this function depending on the length of a and b.
+ * */
+static DfObject*
+int_multiply(DfObject *a, DfObject *b)
+{
+    DfIntObj *x = (DfIntObj *)a;
+    DfIntObj *y = (DfIntObj *)b;
+    DfIntObj *p;
+
+    p = int_multiply_s(x, y);
+    p->count = p->count * DF_SGN(x->count) *
+               DF_SGN(y->count);
+
+    return (DfObject *)p;
+}
+
+/* Make copy of x. */
+static DfIntObj*
+int_copy(DfIntObj *x)
+{
+    int i;
+    int size_x = DF_ABS(x->count);
+    DfIntObj *z = (DfIntObj *)df_int_obj_init(size_x);
+    if (z == NULL)
+        return NULL;
+
+    for (i = 0; i < size_x; ++i)
+        z->digits[i] = x->digits[i];
+
+    z->count = x->count;
+
+    return z;
+}
+
+static DfObject*
+int_abs(DfObject *a)
+{
+    DfIntObj *x = (DfIntObj *)a;
+    DfIntObj *z;
+
+    z = int_copy(x);
+    if (z == NULL)
+        return NULL;
+    z->count = z->count < 0 ? -z->count : z->count;
+
+    return (DfObject *)z;
+}
+
+/* bignum + digit. Make sure a is big enough to hold a*b! */
+static void
+int_add_digit(DfIntObj *a, df_digit b)
+{
+    int i;
+    df_digit carry = b;
+    int size = DF_ABS(a->count);
+
+    for (i = 0; i < size; ++i)
+    {
+       a->digits[i] = a->digits[i] + carry;
+       carry = a->digits[i] >> DF_INT_BITS;
+       a->digits[i] &= DF_INT_MASK;
+    }
+    a->digits[i] = carry;
+}
+
+/* bignum * digit. Make sure a is big enough to hold a*b! */
+static void
+int_mult_digit(DfIntObj *a, df_digit b)
+{
+    int i;
+    df_word carry;
+    df_digit c = 0;
+    int size = DF_ABS(a->count);
+
+    for (i = 0; i < size; ++i)
+    {
+        carry = (df_word)a->digits[i] * (df_word)b + (df_word)c;
+        a->digits[i] = carry & DF_INT_MASK;
+        c = carry >> DF_INT_BITS;
+    }
+    a->digits[i] = c;
+}
+
+/* bignum div digit, bignum mod digit */
+static void
+int_div_digit(DfIntObj *x, df_digit b, df_digit *mod)
+{
+    int size_x = DF_ABS(x->count);
+    df_word z = 0;
+    df_digit q;
+    int i;
+
+    for (i = size_x; i-- > 0; )
+    {
+        q = 0;
+        z = (z << (df_word)DF_INT_BITS) | (df_word)x->digits[i];
+        if (z >= b)
+        {
+            q = (df_digit)(z / b);
+            z -= (df_word)q * (df_word)b;
+        }
+        x->digits[i] = q;
+    }
+
+    *mod = (df_digit)z;
+}
+
+/* Let X be an integer. Let V be a number of digits
+ * in X in base b. W = number of digits in X in base B
+ * can be found by:
+ *                 W = V * (ln(b)/ln(B)) + 1.
+ * B = 2^n => ln(b) = n*ln(2)
+ * => q = ln(b)/ln(2) can be precalcuated. So we can found V
+ * as round up of V*q + 1.
+ * */
+#define DF_SIZE_FROM_DEC(_len) (int)(_len * 3.32192 / DF_INT_BITS + 1 + 0.5)
+#define DF_SIZE_TO_DEC(_len) (int)(_len * DF_INT_BITS * 0.30103 + 1 + 0.5)
+
+DfObject*
+df_int_from_str(char *digits)
+{
+    /* TODO: support different radices */
+    int b = 10; /* digits are decimal */
+    int i;
+    int len = strlen(digits);
+    int size = DF_SIZE_FROM_DEC(len);
+
+    DfIntObj *x = (DfIntObj *)df_int_obj_init(size);
+
+    /* Just evaluate the number */
+    x->digits[0] = digits[0] - '0';
+    for (i = 1; i < len; ++i)
+    {
+        int_mult_digit(x, b);
+        int_add_digit(x, digits[i] - '0');
+    }
+
+    return (DfObject *)remove_leading_zeros(x);
+}
+
+static int
+is_zero(DfIntObj *x)
+{
+    int size_x = DF_ABS(x->count);
+    int i;
+
+    for (i = 0; i < size_x; ++i)
+    {
+        if (x->digits[i] != 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+static char*
+int_to_decimal_str(DfIntObj *x)
+{
+    int size_x = DF_ABS(x->count);
+    int len = DF_SIZE_TO_DEC(size_x);
+    int count = 0;
+    int i;
+    int k;
+    int neg = 0;
+    char *str;
+    df_digit mod = -1;
+    df_digit b = 10;
+    DfIntObj *tmp = int_copy(x);
+    if (tmp == NULL)
+        return NULL;
+
+    df_digit *dec = DF_MEM_ALLOC(len * sizeof(df_digit));
+    df_digit *to_free = dec;
+    if (dec == NULL)
+        return NULL;
+
+    for (k = 0; !is_zero(tmp); )
+    {
+        int_div_digit(tmp, b, &mod);
+        dec[k++] = mod;
+        ++count;
+    }
+
+    if (x->count < 0)
+    {
+        ++count;
+        neg = 1;
+    }
+
+    str = DF_MEM_ALLOC((count + 1) * sizeof(char));
+    if (str == NULL)
+        return NULL;
+
+    i = 0;
+    if (neg)
+    {
+        str[0] = '-';
+        i = 1;
+    }
+    for (; i < count; ++i)
+        str[i] = dec[count - 1 - i] + '0';
+
+    str[i] = '\0';
+
+    DF_MEM_FREE(to_free);
+    df_obj_destroy((DfObject *)tmp);
+    return str;
+}
+
+static int
+int_print(DfObject *a)
+{
+    char *str = int_to_decimal_str((DfIntObj *)a);
+    if (str == NULL)
+        return ENOMEM;
+
+    printf("%s", str);
+    DF_MEM_FREE(str);
+    return 0;
+}
+
+static int
+int_length(DfObject *a)
+{
+    return ((DfIntObj *)a)->count;
+}
+
+static void
+int_destroy(DfObject *a)
+{
+    DfIntObj *x = (DfIntObj *)a;
+
+    DF_MEM_FREE_ARRAY(df_digit, x->digits, x->capacity);
+    DF_MEM_FREE(x);
+}
+
+static DfNumOps as_numeric = {
+    (unaryop)int_negate,
+    (unaryop)int_abs,
+    (binaryop)int_add,
+    (binaryop)int_sub,
+    (binaryop)int_multiply,
+    NULL
+};
+
+static DfContOps as_container = {
+    (intunaryop)int_length,
+    NULL
+};
+
+DfType DfIntType = {
+    "integer",
+    sizeof(DfIntObj),
+    DF_WSIZE/16,
+    (voidunaryop)int_destroy,
+    NULL,
+    (intunaryop)int_print,
+    NULL,
+    NULL,
+    NULL,
+    &as_numeric,
+    &as_container,
+    NULL
+};

tests/api/int.c
@@ -1,0 +1,44 @@
+#include <stdio.h>
+#include "difengine.h"
+#include "intobject.h"
+#include "objectops.h"
+
+int main(int argc, char *argv[])
+{
+    DfObject *a = df_int_from_str("53923543891348405834500421932457932476387");
+    DfObject *b = df_int_from_str("13845648293248349438457293245905734012349");
+    DfObject *y;
+    DfObject *z;
+    DfObject *s;
+
+    printf("a = ");
+    df_obj_print(a, 0);
+    printf(";\n");
+    printf("b = ");
+    df_obj_print(b, 0);
+    printf(";\n");
+
+    printf("a * b = ");
+    y = df_num_multiply(a, b);
+    df_obj_print(y, 0);
+    printf(";\n");
+
+    printf("a + a*b = ");
+    z = df_num_add(a, y);
+    df_obj_print(z, 0);
+    printf(";\n");
+
+    printf("a - a*b = ");
+    s = df_num_sub(a, y);
+    df_obj_print(s, 0);
+    printf(";\n");
+
+    df_obj_destroy(a);
+    df_obj_destroy(b);
+    df_obj_destroy(y);
+    df_obj_destroy(z);
+    df_obj_destroy(s);
+    printf("\nRES: Success\n");
+
+    return 0;
+}

difengine/dis.c
@@ -22,7 +22,9 @@ df_dis_load_const(const char *name, DfCodeObj *code, int offset)
     const_obj = code->consts->items[const_index];
 
     /* There are no types yet. So assume that const_obj is of int type. */
-    printf("%s %4d %d\n", name, const_index, ((DfIntObj *)const_obj)->val);
+    printf("%s %4d ", name, const_index);
+    df_int_print_digits(const_obj);
+    printf("\n");
 
     return offset + 2;
 }

difengine/evaluate.c
@@ -39,7 +39,10 @@ df_eval_frame(DfFrameObj *frame)
          */
         printf("----\n");
         for (DfObject **value = frame->stack; value < frame->stack_top; value++)
-            printf("[ %d ]\n", ((DfIntObj *)*value)->val);
+        {
+            df_int_print_digits(*value);
+            printf("\n");
+        }
 #endif
         opcode = NOPCODE();
         if (HAS_ARG(opcode))

difengine/objectops.c
@@ -2,28 +2,52 @@
 #include "intobject.h"
 #include "memory.h"
 
+/* High level numeric operations */
 DfObject*
 df_num_negative(DfObject *a)
 {
-    DfIntObj *negative = df_int_obj_new(-((DfIntObj *)a)->val);
+    if (a->type->as_numeric != NULL && a->type->as_numeric->neg != NULL)
+        return (*a->type->as_numeric->neg)(a);
 
-    return (DfObject *)negative;
+    /* TODO: Handle error */
+    return NULL;
 }
 
 DfObject*
 df_num_add(DfObject *a, DfObject *b)
 {
-    DfIntObj *sum = df_int_obj_new(((DfIntObj *)a)->val +
-                                   ((DfIntObj *)b)->val);
+    /* TODO: type checks must be more complex
+     * to operate args of different types */
+    if (a->type->as_numeric != NULL && a->type->as_numeric->add != NULL &&
+        a->type == b->type)
+        return (*a->type->as_numeric->add)(a, b);
 
-    return (DfObject *)sum;
+    /* TODO: Handle error */
+    return NULL;
+}
+
+DfObject*
+df_num_sub(DfObject *a, DfObject *b)
+{
+    /* TODO: type checks must be more complex
+     * to operate args of different types */
+    if (a->type->as_numeric != NULL && a->type->as_numeric->sub != NULL &&
+        a->type == b->type)
+        return (*a->type->as_numeric->sub)(a, b);
+
+    /* TODO: Handle error */
+    return NULL;
 }
 
 DfObject*
 df_num_multiply(DfObject *a, DfObject *b)
 {
-    DfIntObj *multiply = df_int_obj_new(((DfIntObj *)a)->val *
-                                        ((DfIntObj *)b)->val);
+    /* TODO: type checks must be more complex
+     * to operate args of different types */
+    if (a->type->as_numeric != NULL && a->type->as_numeric->mul != NULL &&
+        a->type == b->type)
+        return (*a->type->as_numeric->mul)(a, b);
 
-    return (DfObject *)multiply;
+    /* TODO: Handle error */
+    return NULL;
 }

include/defines.h
@@ -15,6 +15,16 @@ extern "C" {
  */
 #define UNUSED(_x) (void)(_x)
 
+/*
+ * Get absolute value.
+ */
+#define DF_ABS(_x) ((_x >= 0) ? _x : -_x)
+
+/*
+ * signum(x).
+ */
+#define DF_SGN(_x) ((_x > 0) ? 1 : (_x == 0) ? 0 : -1)
+
 #ifdef __cplusplus
 }
 #endif

include/difengine.h
@@ -12,6 +12,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <errno.h>
 
 #define DF_DEBUG
 

include/intobject.h
@@ -13,18 +13,49 @@
 extern "C" {
 #endif
 
+#if DF_WSIZE == 64
+typedef uint32_t df_digit;
+typedef uint64_t df_word;
+#define DF_INT_BITS 30
+#define DF_INT_DIGIT_SIZE 32
+#elif DF_WSIZE == 32
+typedef uint16_t df_digit;
+typedef uint32_t df_word;
+#define DF_INT_BITS 15
+#define DF_INT_DIGIT_SIZE 16
+#endif
+#define DF_INT_BASE ((df_digit)1 << DF_INT_BITS)
+#define DF_INT_MASK (DF_INT_BASE - (df_digit)1)
+
 typedef struct {
-    DfObject obj;
-    int val;
+    DfObject_CONT_HEAD;
+    df_digit *digits;
 } DfIntObj;
 
+extern DfType DfIntType;
+
 /**
- * Create new integer object.
+ * Create an integer object with <size> digits = 0.
  *
  * @return
  *   new integer object
  */
-extern DfIntObj* df_int_obj_new(int val);
+extern DfObject* df_int_obj_init(int size);
+
+/**
+ * Create an integer object from string of digits.
+ * Works only for decimal strings now.
+ *
+ * @return
+ *   new integer object
+ */
+extern DfObject* df_int_from_str(char *digits);
+
+/**
+ * Print number's digits in  DF_INT_BASE.
+ * For debug purposes only.
+ */
+extern void df_int_print_digits(DfObject *a);
 
 #ifdef __cplusplus
 }

commit dcf6615c174d259752335632ccf6be2862773173

object: int: handle the minus sign in input string
Fixes: a39dbe204bf8 ("object: int: add integer type")
Cc: dilshod@difengine.com

difengine/intobject.c
@@ -420,18 +420,25 @@ df_int_from_str(char *digits)
     /* TODO: support different radices */
     int b = 10; /* digits are decimal */
     int i;
+    int neg = 0;
     int len = strlen(digits);
     int size = DF_SIZE_FROM_DEC(len);
 
     DfIntObj *x = (DfIntObj *)df_int_obj_init(size);
 
+    if (digits[0] == '-')
+       neg = 1;
+
     /* Just evaluate the number */
-    x->digits[0] = digits[0] - '0';
+    x->digits[0] = digits[neg] - '0';
     for (i = 1; i < len; ++i)
     {
         int_mult_digit(x, b);
         int_add_digit(x, digits[i] - '0');
     }
+
+    if (neg)
+        x->count = -x->count;
 
     return (DfObject *)remove_leading_zeros(x);
 }

tests/api/int.c
@@ -5,8 +5,8 @@
 
 int main(int argc, char *argv[])
 {
-    DfObject *a = df_int_from_str("53923543891348405834500421932457932476387");
-    DfObject *b = df_int_from_str("13845648293248349438457293245905734012349");
+    DfObject *a = df_int_from_str("-592354389134840583450042193245793247638");
+    DfObject *b = df_int_from_str("384564829324834943845729324590573401234");
     DfObject *y;
     DfObject *z;
     DfObject *s;