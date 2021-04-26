urazoff/difengine

commit 8e222f03fb296f12af177e40e1ecfe72559eb3e2

object: complex: add minimal support of complex numbers

difengine/complexobject.c
@@ -1,0 +1,158 @@
+/* for hypot() */
+#include <math.h>
+#include "complexobject.h"
+#include "floatobject.h"
+#include "defines.h"
+#include "memory.h"
+
+DfObject*
+df_complex_obj_init(double real, double im)
+{
+    DfComplexObj *x = (DfComplexObj *)df_obj_new(&DfComplexType);
+
+    if (x == NULL)
+        return NULL;
+
+    x->real = real;
+    x->im = im;
+
+    return (DfObject *)x;
+}
+
+static DfObject*
+complex_negate(DfObject *a)
+{
+    DfComplexObj *x = (DfComplexObj *)a;
+
+    return df_complex_obj_init(-x->real, -x->im);
+}
+
+static DfObject*
+complex_abs(DfObject *a)
+{
+    DfComplexObj *x = (DfComplexObj *)a;
+
+    return df_float_obj_init(hypot(x->real, x->im));
+}
+
+/* Actually only check for equality is senseful
+ * but we need to return something if a != b.
+ * */
+static int
+complex_compare(DfObject *a, DfObject *b)
+{
+
+    DfComplexObj *x = (DfComplexObj *)a;
+    DfComplexObj *y = (DfComplexObj *)b;
+
+    if (x->real == y->real)
+    {
+        if (x->im == y->im)
+            return 0;
+        else
+            return (x->im < y->im) ? -1 : 1;
+    }
+    else
+    {
+        return (x->real < y->real) ? -1 : 1;
+    }
+}
+
+static DfObject*
+complex_add(DfObject *a, DfObject *b)
+{
+    DfComplexObj *x = (DfComplexObj *)a;
+    DfComplexObj *y = (DfComplexObj *)b;
+
+    return df_complex_obj_init(x->real + y->real, x->im + y->im);
+}
+
+static DfObject*
+complex_sub(DfObject *a, DfObject *b)
+{
+    DfComplexObj *x = (DfComplexObj *)a;
+    DfComplexObj *y = (DfComplexObj *)b;
+
+    return df_complex_obj_init(x->real - y->real, x->im - y->im);
+}
+
+static DfObject*
+complex_multiply(DfObject *a, DfObject *b)
+{
+    DfComplexObj *x = (DfComplexObj *)a;
+    DfComplexObj *y = (DfComplexObj *)b;
+
+    return df_complex_obj_init(x->real * y->real - x->im * y->im,
+                               x->real * y->im + x->im * y->real);
+}
+
+static DfObject*
+complex_divide(DfObject *a, DfObject *b)
+{
+    DfComplexObj *x = (DfComplexObj *)a;
+    DfComplexObj *y = (DfComplexObj *)b;
+    double r2;
+
+    /* TODO: handle the error */
+    if (y->real == 0.0 && y->im == 0.0)
+        return NULL;
+
+    r2 = y->real * y->real + y->im * y->im;
+
+    return df_complex_obj_init((x->real * y->real + x->im * y->im)/r2,
+                               (x->im * y->real - x->real * y->im)/r2);
+}
+
+static int
+complex_print(DfObject *a)
+{
+    DfComplexObj *x = (DfComplexObj *)a;
+
+    /* Imaginary number */
+    if (x->real == 0.0)
+        printf("%lfi", x->im);
+    else
+        printf("%lf + %lfi", x->real, x->im);
+
+    return 0;
+}
+
+/* TODO: proper hash function which coincides with
+ * hash of int if values are the same.
+ */
+static uint32_t
+complex_hash(DfObject *a)
+{
+    UNUSED(a);
+    return 0;
+}
+
+static void
+complex_destroy(DfObject *a)
+{
+    DF_MEM_FREE(a);
+}
+
+static DfNumOps as_numeric = {
+    (unaryop)complex_negate,
+    (unaryop)complex_abs,
+    (binaryop)complex_add,
+    (binaryop)complex_sub,
+    (binaryop)complex_multiply,
+    (binaryop)complex_divide,
+};
+
+DfType DfComplexType = {
+    "complex",
+    sizeof(DfComplexObj),
+    0,
+    (voidunaryop)complex_destroy,
+    (hashop)complex_hash,
+    (intunaryop)complex_print,
+    (intbinaryop)complex_compare,
+    NULL,
+    NULL,
+    &as_numeric,
+    NULL,
+    NULL
+};

difengine/meson.build
@@ -37,6 +37,11 @@ float_lib = static_library('float',
                            include_directories: global_inc,
                            link_with: mem_lib)
 
+complex_lib = static_library('complex',
+                             'complexobject.c',
+                             include_directories: global_inc,
+                             link_with: [mem_lib, float_lib])
+
 ops_lib = static_library('ops',
                          'objectops.c',
                           include_directories: global_inc,
@@ -70,6 +75,7 @@ df_libs += [
             frame_lib,
             int_lib,
             float_lib,
+            complex_lib,
             ops_lib,
             eval_lib,
             list_lib,

front/lexer.c
@@ -354,6 +354,10 @@ df_lexer_get(DfLexer *lexer, const char **start, const char **end)
             } while (is_digit(c));
         }
 
+        /* Imaginary number */
+        if (c == 'i')
+            df_lexer_nextc(lexer);
+
         df_lexer_back(lexer);
         *start = lexer->start;
         *end = lexer->cur;

include/complexobject.h
@@ -1,0 +1,36 @@
+#ifndef _DF_COMPLEXOBJECT_H_
+#define _DF_COMPLEXOBJECT_H_
+
+#include "object.h"
+#include "difengine.h"
+
+/**
+ * @file
+ * Complex numbers with double-precision floating point parts.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    DfObject obj;
+    double real;
+    double im;
+} DfComplexObj;
+
+extern DfType DfComplexType;
+
+/**
+ * Create complex object from two doubles.
+ *
+ * @return
+ *   new complex object
+ */
+extern DfObject* df_complex_obj_init(double real, double im);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DF_COMPLEXOBJECT_H_ */

commit a7f49a0140b9b62b0d7afe58184e09db35abadb2

object: complex: fix printing when imaginary part is negative
Fixes: 8e222f03fb29 ("object: complex: add minimal support of complex numbers")
Cc: dilshod@difengine.com

difengine/complexobject.c
@@ -112,7 +112,7 @@ complex_print(DfObject *a)
     if (x->real == 0.0)
         printf("%lfi", x->im);
     else
-        printf("%lf + %lfi", x->real, x->im);
+        printf("%lf%+lfi", x->real, x->im);
 
     return 0;
 }