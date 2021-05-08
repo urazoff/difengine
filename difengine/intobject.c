urazoff/difengine

commit dcf6615c174d259752335632ccf6be2862773173

object: int: handle the minus sign in input string
Fixes: a39dbe204bf8 ("object: int: add integer type")
Cc: dilshod@difengine.com

difengine/intobject.c
@@ -420,18 +420,25 @@ df_int_from_str(char *digits)
     /* TODO: support different radices */
     int b = 10; /* digits are decimal */
     int i;
+    int neg = 0;
     int len = strlen(digits);
     int size = DF_SIZE_FROM_DEC(len);
 
     DfIntObj *x = (DfIntObj *)df_int_obj_init(size);
 
+    if (digits[0] == '-')
+       neg = 1;
+
     /* Just evaluate the number */
-    x->digits[0] = digits[0] - '0';
+    x->digits[0] = digits[neg] - '0';
     for (i = 1; i < len; ++i)
     {
         int_mult_digit(x, b);
         int_add_digit(x, digits[i] - '0');
     }
+
+    if (neg)
+        x->count = -x->count;
 
     return (DfObject *)remove_leading_zeros(x);
 }

tests/api/int.c
@@ -5,8 +5,8 @@
 
 int main(int argc, char *argv[])
 {
-    DfObject *a = df_int_from_str("53923543891348405834500421932457932476387");
-    DfObject *b = df_int_from_str("13845648293248349438457293245905734012349");
+    DfObject *a = df_int_from_str("-592354389134840583450042193245793247638");
+    DfObject *b = df_int_from_str("384564829324834943845729324590573401234");
     DfObject *y;
     DfObject *z;
     DfObject *s;

commit 7395eabb92eb2f186ed09e3a472ef96d0b46ff29

object: int: fix handling the minus sign in input string
Fixes: dcf6615c174d ("object: int: handle the minus sign in input string")
Cc: dilshod@difengine.com

difengine/intobject.c
@@ -431,7 +431,7 @@ df_int_from_str(char *digits)
 
     /* Just evaluate the number */
     x->digits[0] = digits[neg] - '0';
-    for (i = 1; i < len; ++i)
+    for (i = neg + 1; i < len; ++i)
     {
         int_mult_digit(x, b);
         int_add_digit(x, digits[i] - '0');